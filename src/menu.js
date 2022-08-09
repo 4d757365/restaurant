5373ac0a8271e29c0e3b9548de84534a37e23afd
Muse Tesfaye <tesfaye.musee@gmail.com>
2022-08-08 22:19:06 -0400

    Completed menu page Added the menu page and menu items including basic styling.

--- src/menu.js
+++ src/menu.js
@@ -1,6 +1,41 @@
 export default function menu() {
-    const bodyDiv = document.createElement('h1');
-    bodyDiv.innerText = 'Menu';
+    const $content = document.createElement('div');
+    $content.setAttribute('id', 'menu');
+    $content.innerHTML = 
+    `
+        <div class="menu-items">
+            <div class="item">
+            <h3>Pizza Classica</h3>
+            <img src="/assets/classic.jpeg" alt="Pizza">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </div>
+            <div class="item">
+                <h3>Soppressata Pizza</h3>
+                <img src="/assets/soppressata.jpeg" alt="Pizza">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </div>
+            <div class="item">
+                <h3>Classic Cheese Pizza</h3>
+                <img src="/assets/classic.jpeg" alt="Pizza">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </div>
+            <div class="item">
+                <h3>Margherita Pizza</h3>
+                <img src="/assets/margherita.jpeg" alt="Pizza">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </div>
+            <div class="item">
+                <h3>Sausage and Ricotta Pizza</h3>
+                <img src="/assets/sausage.jpeg" alt="Pizza">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </div>
+            <div class="item">
+                <h3>Wild Mushroom Pizza</h3>
+                <img src="/assets/mushroom.jpeg" alt="Pizza">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </div>
+        </div>
+    `;
 
-    return bodyDiv;
+    return $content;
 }
